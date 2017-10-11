f04bd77c823f167fa582f41ffc9c481e7d0c0453
root <root@Hopi-PC.localdomain>
2017-10-11 14:12:16 +1100

    Changed font style to digital font in input text field on login page and resized input form size

--- login.php
+++ login.php
@@ -1,9 +1,7 @@
-
 <?php require './inc/header.php'; ?>
-
 <?php require './inc/jumbotron.php'; ?>
 
-<div class="container text-center col-6">
+<div class="container text-center col-10 col-sm-4 col-lg-3 col-xl-2">
 
       <form action="./index.php" method="post" class="form">
         <div id="formGroup" class="form-group">
@@ -11,16 +9,12 @@
           <label for="id" class="sr-only">Ethos ID</label>
           <div class="inner-addon right-addon">
             <i id="spinner" class="fa fa-spinner text-primary" aria-hidden="true"></i>
-            <input type="text" id="id" name="id" class="form-control" placeholder="Your Ethos ID" minlength="6" maxlength="6" required autofocus>
+            <input type="text" id="id" name="id" class="form-control" placeholder="Ethos ID" minlength="6" maxlength="6" required autofocus>
           </div>
-          <button id="submit" class="btn btn-primary btn-block mt-2" type="submit" disabled>Sign in</button>
-
-
+          <button id="submit" class="btn btn-lg btn-primary btn-block mt-2" type="submit" disabled>Sign in</button>
         </div>
       </form>
 
-    </div> <!-- /container -->
-
-
+</div> <!-- /container -->
 
 <?php require './inc/footer.php'; ?>
